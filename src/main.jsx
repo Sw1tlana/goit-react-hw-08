cfed793ea60f87c734b9cd26c2bf4c032e54fe55
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-04-08 11:22:07 +0300

    Added structure project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "modern-normalize";
+import App from './App.jsx'
+import './index.css'
+
+import { Provider } from 'react-redux'
+import { store, persistor } from './redux/store.js'
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+       <PersistGate persistor={persistor} loading={null}>
+         <BrowserRouter>
+           <App />
+          </BrowserRouter>
+       </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+)
